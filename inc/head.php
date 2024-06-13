0675b831820071e37e0aaa67b6a2fc13a73af393
22DP1RMiga <131353118+22DP1RMiga@users.noreply.github.com>
2024-06-13 06:52:42 +0000

    Papildus failu mācību stundām

---
+++ inc/head.php
@@ -0,0 +1,7 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portfolio</title>
+    <link rel="stylesheet" href="assets/styles.css">
+    <script src="assets/main.py" defer></script>
+</head>
